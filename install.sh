a16a20c349c2c43d729a09b39cfc74f1583d253a
Sara Vallero <svallero@to.infn.it>
2020-08-06 15:23:12 +0200

    More general scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 helm repo add gitlab https://charts.gitlab.io
 
-helm install --namespace gitlab --name gitlab-runner-test -f values_test.yaml gitlab/gitlab-runner
-#helm upgrade --namespace gitlab gitlab-runner-test -f values_test.yaml gitlab/gitlab-runner
+tags="kube-yoga"
+# We want to register a different runner for each project
+project="hello-world"
+# This is the project's token to add runners
+# (get it from Settings->CI/CD->Runners)
+token="FHqPAkZbkWse-fj7SBcC"
+
+# Write the helm config file
+cat <<EOF > values-${project}.yaml
+gitlabUrl: https://git.ligo.org/
+runnerRegistrationToken: ${token}
+rbac:
+  create: false
+  serviceAccountName: runner
+runners:
+  privileged: true
+  tags: ${tags}
+EOF
+
+helm install --namespace gitlab --name $project -f values-${project}.yaml gitlab/gitlab-runner
+#helm upgrade --namespace gitlab $project -f values-${project}.yaml gitlab/gitlab-runner
 
